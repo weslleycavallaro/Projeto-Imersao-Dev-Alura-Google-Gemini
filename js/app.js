0ec599199db1e5b30742f616982a7e3e45cfe70c
Weslley Cavallaro <90354207+weslleycavallaro@users.noreply.github.com>
2024-09-06 15:24:45 -0300

    Adding changes
    Aperfeiçoando o JavaScript

--- js/app.js
+++ js/app.js
@@ -1,24 +1,62 @@
 let main = "";
+let erro = `
+
+    <section class="main">
+        <h3>Nenhum índice encontrado!</h3>\
+        <h3>Tente alguma palavra mais genérica.</h3>
+    </section>
+
+`
 
 function search(){
 
     let secao = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
     main = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
-    for(let i of carros){
-        main += `
+    if(campoPesquisa != ""){
 
-            <div class="item-resultado">
-                <img src="${i.imagem}" alt="${i.titulo}">
-                <h2>${i.titulo}</h2>
-                <p class="descricao-meta">${i.descricao}</p>
-                <a href="${i.link}" target="_blank"><button>Read more</button></a>
-            </div>
+        campoPesquisa = campoPesquisa.toLowerCase();
 
-        `
+        for(let i of carros){
+
+            titulo = i.titulo.toLowerCase();
+            descricao = i.descricao.toLowerCase();
+            tags = i.tags.toLowerCase();
+
+            if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+
+            main += `
+
+                <div class="item-resultado">
+                    <img src="${i.imagem}" alt="${i.titulo}">
+                    <h2>${i.titulo}</h2>
+                    <p class="descricao-meta">${i.descricao}</p>
+                    <a href="${i.link}" target="_blank"><button>Read more</button></a>
+                </div>
+
+            `
+            }
+        }
+        
     }
-    secao.innerHTML = main;
+
+    if(main != ""){
+
+        secao.innerHTML = main;
+
+    }else{
+
+        secao.innerHTML = erro;
+        
+    }
+
+    
 }
 
 function clean(){
--- js/dados.js
+++ js/dados.js
@@ -3,37 +3,43 @@ let carros = [
         titulo: "Porsche 911 Carrera RS",
         descricao: "Lançada em 1973, a Carrera RS era uma máquina de corrida homologada para as ruas, com um design marcante e um motor potente. Seu foco em leveza e desempenho a tornou um carro extremamente ágil e divertido de dirigir. A cauda de pato e o interior despojado são características distintivas desse modelo. A Carrera RS deixou um legado duradouro na indústria automobilística e continua sendo um dos carros esportivos mais desejados por colecionadores, com seus modelos originais atingindo valores astronômicos em leilões.",
         link: "https://quatrorodas.abril.com.br/noticias/classicos-porsche-911-carrera-rs-tecnica-e-criatividade",
-        imagem: "image/porsche911_carrerars.jpg"
+        imagem: "image/porsche911_carrerars.jpg",
+        tags: "porche porsche carro 911 turbo veiculo"
     },
     {
         titulo: "Ferrari 250 GTO",
         descricao: "A Ferrari 250 GTO, produzida em um número extremamente limitado entre 1962 e 1964, é considerada um dos carros mais icônicos e valiosos do mundo. Com um design elegante e um motor V12 potente, a 250 GTO dominou as pistas e conquistou o coração dos colecionadores. Sua raridade, desempenho e beleza a transformaram em um investimento seguro e um objeto de desejo para os apaixonados por carros clássicos. A 250 GTO representa um momento único na história da Ferrari e do automobilismo, transcendo o tempo e continuando a encantar gerações de entusiastas.",
         link: "https://www.ferrari.com/en-EN/auto/250-gto",
-        imagem: "image/ferrari_250gto.jpg"
+        imagem: "image/ferrari_250gto.jpg",
+        tags: "ferari ferrarri vermelho cavalo"
     },
     {
         titulo: "Mercedes 350 SL",
         descricao: "A Mercedes-Benz 350 SL é um ícone do design automobilístico, combinando elegância e performance de forma única. Produzida entre 1971 e 1989, a 350 SL se tornou sinônimo de luxo e sofisticação, conquistando admiradores em todo o mundo. Seu motor V8, aliado a um chassi robusto e um design atemporal, proporcionava uma experiência de condução suave e prazerosa. A capota retrátil permitia desfrutar do ar livre, tornando-a um carro perfeito para longas viagens e momentos especiais. A 350 SL rapidamente se tornou um símbolo de status e continua sendo um dos modelos mais desejados pelos colecionadores de carros clássicos, preservando seu legado como um verdadeiro clássico atemporal.",
         link: "https://www.gettyimages.com.br/fotos/mercedes-350-sl",
-        imagem: "image/mercedes_350sl.jpg"
+        imagem: "image/mercedes_350sl.jpg",
+        tags: "mercede conversivel capota"
     },
     {
         titulo: "Opala Diplomata",
         descricao: "O Opala Diplomata, um modelo icônico da Chevrolet, marcou época no Brasil com seu design elegante e desempenho robusto. Produzido por muitos anos, o Diplomata se tornou sinônimo de status e conforto, conquistando o coração de gerações de brasileiros. Com suas diversas versões e motorizações, o Opala Diplomata deixou um legado duradouro na indústria automobilística nacional, sendo até hoje um dos carros mais cobiçados pelos amantes de carros clássicos.",
         link: "https://quatrorodas.abril.com.br/carros-classicos/grandes-brasileiros-chevrolet-opala-diplomata-se",
-        imagem: "image/opala_diplomata.jpg"
+        imagem: "image/opala_diplomata.jpg",
+        tags: "v6 6 cilindros brasil"
     },
     {
         titulo: "Gol GTi 84",
         descricao: "O Gol GTi 84, lançado em um momento de efervescência no mercado automobilístico brasileiro, rapidamente se tornou um ícone para os apaixonados por carros esportivos. Com seu design compacto e ágil, combinado a um motor potente e um interior esportivo, o GTi 84 conquistou o coração dos jovens da época e deixou sua marca na história da Volkswagen.",
         link: "https://quatrorodas.abril.com.br.carros-classicos/classicos-vw-gol-gt-o-carro-que-converteu-o-fracasso-do-hatch-em-sucesso",
-        imagem: "image/gol_gti.jpg"
+        imagem: "image/gol_gti.jpg",
+        tags: "gol quadrado brasil"
     },
     {
         titulo: "Fusca 1300L",
         descricao: "O Fusca 1300L, um ícone da indústria automobilística mundial, conquistou o coração dos brasileiros por sua simplicidade, robustez e versatilidade. Lançado no Brasil em 1967, o 1300L se tornou sinônimo de liberdade e aventura, marcando presença em diversas gerações. Com seu motor 1300cc e design atemporal, o Fusca 1300L continua sendo um qual dos carros mais queridos pelos brasileiros.",
         link: "https://www.uol.com.br/carros/noticias/fusca-1300l-o-carro-que-marcou-epoca-no-brasil.htm",
-        imagem: "image/fusca_1300l.jpg"
+        imagem: "image/fusca_1300l.jpg",
+        tags: "brasil fusquinha"
     }
 ];
 
